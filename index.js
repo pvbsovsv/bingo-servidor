04a33aa4fdd0cce6e82085c482bd5ebf0bec1ff5
pvbsovsv <pablo.a.sousa@gmail.com>
2025-07-11 23:29:50 +0100

    cors package installed

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors'); // <-- 
+
+const app = express();
+const port = 3000;
+
+// Enable CORS for all origins
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('')
