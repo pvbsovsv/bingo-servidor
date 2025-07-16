8e84a83f069a92743320f1a416ab375f755b8166
pvbsovsv <pablo.a.sousa@gmail.com>
2025-07-16 16:32:44 +0100

    final

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ let arrayFrases = [
 ];
 
 // Now define your routes
-
+//root 
 app.get('/', (req, res) => {
   res.send('');
 });
@@ -100,10 +100,6 @@ app.post('/frases', (req, res) => {
   }
 });
 
-// Root
-app.get('/', (req, res) => {
-  res.send('');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
